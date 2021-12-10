586ac8f69e7670892c7120eb7f68f097118cebdb
David Moxey <dave@xyloid.org>
2021-12-10 18:16:25 +0000

    nektar (5.1.0): bump version
    Closes #3.
    
    Signed-off-by: David Moxey <dave@xyloid.org>

--- Formula/nektar.rb
+++ Formula/nektar.rb
@@ -1,153 +1,203 @@
-# Nektar++ will, by default, download and build its ThirdParty
-# dependencies. This download strategy has been added so that Homebrew can
-# download resources and check hashes independently.
-class NektarThirdPartyDownloadStrategy < CurlDownloadStrategy
-  def stage
-    cp cached_location, basename_without_params
-  end
-end
-
 class Nektar < Formula
-  desc "Nektar++ spectral/hp element framework"
+  desc "High-performance spectral/hp element framework"
   homepage "https://www.nektar.info/"
-  #url "http://www.nektar.info/downloads/file/nektar-4.3.4.tar.gz"
-  #sha256 "5207f8010d77fbc53c9407698de5426cae90d1a054fecf41ecc04060226d65f5"
-  head "https://gitlab.nektar.info/nektar/nektar.git"
+  url "https://gitlab.nektar.info/nektar/nektar/-/archive/v5.1.0/nektar-v5.1.0.tar.bz2"
+  sha256 "f5fdb729909e4dcd42cb071f06569634fa87fe90384ba0f2f857a9e0e56b6ac5"
+  revision 1
 
-  option "with-demos", "Compile Nektar++ demo executables"
-
-  depends_on "cmake"
+  depends_on "arpack"
   depends_on "boost"
-  depends_on "tinyxml"
-  depends_on "zlib"
+  depends_on "boost-python3"
+  depends_on "cmake"
+  depends_on "fftw"
+  depends_on "hdf5-mpi"
+  depends_on "numpy"
+  depends_on "open-mpi"
   depends_on "opencascade"
   depends_on "scotch"
-  depends_on "hdf5"     => :recommended
-  depends_on "vtk"      => :recommended
-  depends_on "open-mpi" => :recommended
-  depends_on "arpack"   => :recommended
-  depends_on "fftw"     => :recommended
-  depends_on "petsc"    => :recommended
-  depends_on "metis"    => :recommended
+  depends_on "tinyxml"
+  depends_on "zlib"
+
+  # Patch for HDF 1.12
+  patch :DATA
 
   def install
     args = std_cmake_args + ["-DNEKTAR_BUILD_TESTS=OFF",
                              "-DNEKTAR_BUILD_UNIT_TESTS=OFF",
                              "-DZLIB_ROOT=#{Formula["zlib"].opt_prefix}"]
 
-    args << "-DNEKTAR_BUILD_DEMOS=ON"   if build.with?    "demos"
-    args << "-DNEKTAR_BUILD_DEMOS=OFF"  if build.without? "demos"
-    args << "-DNEKTAR_BUILD_MESHGEN=ON" if build.with?    "opencascade"
-    args << "-DNEKTAR_USE_MPI=ON"       if build.with?    :mpi
-    args << "-DNEKTAR_USE_ARPACK=ON"    if build.with?    "arpack"
-    args << "-DNEKTAR_USE_FFTW=ON"      if build.with?    "fftw"
-    args << "-DNEKTAR_USE_VTK=ON"       if build.with?    "vtk"
-    args << "-DNEKTAR_USE_SCOTCH=ON"    if build.with?    "scotch"
-    args << "-DNEKTAR_USE_ARPACK=ON"    if build.with?    "arpack"
-    args << "-DNEKTAR_USE_HDF5=ON"      if build.with?    "hdf5"
+    args << "-DNEKTAR_BUILD_DEMOS=OFF"
+    args << "-DNEKTAR_BUILD_MESHGEN=ON"
+    args << "-DNEKTAR_BUILD_PYTHON=ON"
+    args << "-DNEKTAR_SOLVER_DIFFUSION=OFF"
+    args << "-DNEKTAR_SOLVER_DUMMY=OFF"
+    args << "-DNEKTAR_SOLVER_ELASTICITY=OFF"
+    args << "-DNEKTAR_SOLVER_MMF=OFF"
+    args << "-DNEKTAR_USE_MPI=ON"
+    args << "-DNEKTAR_USE_ARPACK=ON"
+    args << "-DNEKTAR_USE_FFTW=ON"
+    args << "-DNEKTAR_USE_SCOTCH=ON"
+    args << "-DNEKTAR_USE_ARPACK=ON"
+    args << "-DNEKTAR_USE_HDF5=ON"
+    args << "-DPYTHON_EXECUTABLE=#{Formula["python@3.9"].opt_bin}/python3"
 
-    if build.with? "petsc"
-      petscdir = File.join(Formula["petsc"].opt_prefix, "real")
-      args << "-DNEKTAR_USE_PETSC=ON"
-      args << "-DPETSC_DIR=#{petscdir}"
-    end
+    # Compile with C++14 support for boost 1.75+
+    args << "-DCMAKE_CXX_STANDARD=14"
 
     mkdir "build" do
       system "cmake", "..", *args
-      system "make"
-      components = %w[ThirdParty lib solvers util dev]
-      components.each { |c| system "cmake", "-DCOMPONENT=#{c}", "-P", "cmake_install.cmake" }
+      system "make", "install"
+
+      # Also need to install NekPy bindings
+      python = Formula["python@3.9"]
+      system python.bin/"python3", *Language::Python.setup_install_args(prefix)
+
+      site_packages = Language::Python.site_packages(python)
+      pth_contents = "import site; site.addsitedir('#{libexec/site_packages}')\n"
+      (prefix/site_packages/"homebrew-nektar.pth").write pth_contents
     end
   end
 
   test do
-    (testpath/"helm.cpp").write <<-EOS
-#include <iostream>
-#include <MultiRegions/ContField2D.h>
-#include <SpatialDomains/MeshGraph2D.h>
+    (testpath/"helm.cpp").write <<~EOS
+      #include <iostream>
+      #include <MultiRegions/ContField.h>
+      #include <SpatialDomains/MeshGraph.h>
 
-using namespace Nektar;
-using namespace std;
+      using namespace Nektar;
+      using namespace std;
 
-int main(int argc, char *argv[])
-{
-    LibUtilities::SessionReaderSharedPtr vSession
-        = LibUtilities::SessionReader::CreateInstance(argc, argv);
-    SpatialDomains::MeshGraphSharedPtr graph =
-        SpatialDomains::MeshGraph::Read(vSession);
-    MultiRegions::ContField2DSharedPtr fld =
-        MemoryManager<MultiRegions::ContField2D>::AllocateSharedPtr(
-            vSession, graph, vSession->GetVariable(0));
+      int main(int argc, char *argv[])
+      {
+          LibUtilities::SessionReaderSharedPtr vSession
+              = LibUtilities::SessionReader::CreateInstance(argc, argv);
+          SpatialDomains::MeshGraphSharedPtr graph =
+              SpatialDomains::MeshGraph::Read(vSession);
+          MultiRegions::ContFieldSharedPtr fld =
+              MemoryManager<MultiRegions::ContField>::AllocateSharedPtr(
+                  vSession, graph, vSession->GetVariable(0));
 
-    // Set up forcing
-    const int nq = fld->GetNpoints(), nm = fld->GetNcoeffs();
-    Array<OneD, NekDouble> xc0(nq), xc1(nq), xc2(nq);
-    fld->GetCoords(xc0, xc1, xc2);
-    vSession->GetFunction("Forcing", 0)->Evaluate(xc0, xc1, xc2, fld->UpdatePhys());
+          // Set up forcing
+          const int nq = fld->GetNpoints(), nm = fld->GetNcoeffs();
+          Array<OneD, NekDouble> xc0(nq), xc1(nq), xc2(nq);
+          fld->GetCoords(xc0, xc1, xc2);
+          vSession->GetFunction("Forcing", 0)->Evaluate(xc0, xc1, xc2, fld->UpdatePhys());
 
-    // Solve Helmholtz
-    StdRegions::ConstFactorMap factors;
-    FlagList flags;
-    factors[StdRegions::eFactorLambda] = vSession->GetParameter("Lambda");
-    Vmath::Zero(nm, fld->UpdateCoeffs(), 1);
-    fld->HelmSolve(fld->GetPhys(), fld->UpdateCoeffs(), flags, factors);
+          // Solve Helmholtz
+          StdRegions::ConstFactorMap factors;
+          factors[StdRegions::eFactorLambda] = vSession->GetParameter("Lambda");
+          Vmath::Zero(nm, fld->UpdateCoeffs(), 1);
+          fld->HelmSolve(fld->GetPhys(), fld->UpdateCoeffs(), factors);
 
-    // Evaluate L^2 error
-    fld->BwdTrans(fld->GetCoeffs(), fld->UpdatePhys());
-    Array<OneD, NekDouble> sol(nq);
-    vSession->GetFunction("ExactSolution", 0)->Evaluate(xc0, xc1, xc2, sol);
-    cout << fld->L2(fld->GetPhys(), sol) << endl;
-    return 0;
-}
-EOS
+          // Evaluate L^2 error
+          fld->BwdTrans(fld->GetCoeffs(), fld->UpdatePhys());
+          Array<OneD, NekDouble> sol(nq);
+          vSession->GetFunction("ExactSolution", 0)->Evaluate(xc0, xc1, xc2, sol);
+          cout << fld->L2(fld->GetPhys(), sol) << endl;
+          return 0;
+      }
+    EOS
 
-    (testpath/"CMakeLists.txt").write <<-EOS
-find_package(Nektar++ REQUIRED NO_MODULE NO_DEFAULT_PATH NO_CMAKE_BUILDS_PATH NO_CMAKE_PACKAGE_REGISTRY)
-include_directories(${NEKTAR++_INCLUDE_DIRS} ${NEKTAR++_TP_INCLUDE_DIRS})
-add_definitions(${NEKTAR++_DEFINITIONS})
-link_directories(${NEKTAR++_LIBRARY_DIRS} ${NEKTAR++_TP_LIBRARY_DIRS})
-add_executable(helm helm.cpp)
-target_link_libraries(helm ${NEKTAR++_LIBRARIES} ${NEKTAR++_TP_LIBRARIES})
-EOS
+    (testpath/"CMakeLists.txt").write <<~EOS
+      set(CMAKE_CXX_STANDARD 14)
+      find_package(Nektar++ REQUIRED NO_MODULE NO_DEFAULT_PATH NO_CMAKE_BUILDS_PATH NO_CMAKE_PACKAGE_REGISTRY)
+      include_directories(${NEKTAR++_INCLUDE_DIRS} ${NEKTAR++_TP_INCLUDE_DIRS})
+      add_definitions(${NEKTAR++_DEFINITIONS})
+      link_directories(${NEKTAR++_LIBRARY_DIRS} ${NEKTAR++_TP_LIBRARY_DIRS})
+      add_executable(helm helm.cpp)
+      target_link_libraries(helm ${NEKTAR++_LIBRARIES} ${NEKTAR++_TP_LIBRARIES})
+    EOS
 
-    (testpath/"input.xml").write <<-EOS
-<NEKTAR>
-    <GEOMETRY DIM="2" SPACE="2">
-        <VERTEX COMPRESSED="B64Z-LittleEndian" BITSIZE="64">eJxjYEAGD/aj0gwMjAzYAEKeCas8AjDj0AcDLNjNt4exWLG7Dy7PhlUfwh52HObCAAd2/XB1AAerEJkA</VERTEX>
-        <EDGE COMPRESSED="B64Z-LittleEndian" BITSIZE="64">eJxjYEAFjDhoJhw0Mw4aBljQ1MP4rDj4bGh8mHnsaO6BqeNA48PUcaLxYfZzoYnD9HOj8WHuAgBAqACe</EDGE>
-        <ELEMENT>
-            <Q COMPRESSED="B64Z-LittleEndian" BITSIZE="64">eJxjYEAFjFCaCUoz4xBngdKsUJoNTZ4dSnNAaU40c5jRxLmgNDea+QAUwABZ</Q>
-        </ELEMENT>
-        <COMPOSITE>
-            <C ID="0"> Q[0-3] </C>
-            <C ID="1"> E[0,3,5,6,7,8,10,11] </C>
-        </COMPOSITE>
-        <DOMAIN> C[0] </DOMAIN>
-    </GEOMETRY>
-    <EXPANSIONS>
-        <E COMPOSITE="C[0]" NUMMODES="9" TYPE="MODIFIED" FIELDS="u" />
-    </EXPANSIONS>
-    <CONDITIONS>
-        <PARAMETERS> <P> Lambda = 1 </P> </PARAMETERS>
-        <VARIABLES> <V ID="0"> u </V> </VARIABLES>
-        <BOUNDARYREGIONS> <B ID="0"> C[1] </B> </BOUNDARYREGIONS>
-        <BOUNDARYCONDITIONS>
-            <REGION REF="0">
-                <D VAR="u" VALUE="sin(PI*x)*sin(PI*y)" />
-            </REGION>
-        </BOUNDARYCONDITIONS>
-        <FUNCTION NAME="Forcing">
-            <E VAR="u" VALUE="-(Lambda + 2*PI*PI)*sin(PI*x)*sin(PI*y)" />
-        </FUNCTION>
-        <FUNCTION NAME="ExactSolution">
-            <E VAR="u" VALUE="sin(PI*x)*sin(PI*y)" />
-        </FUNCTION>
-    </CONDITIONS>
-</NEKTAR>
-EOS
-    nekpp_version = `#{bin}/ADRSolver --version | awk '{print $3}'`.strip
-    system "cmake", "-DNektar++_DIR=#{lib}/nektar++-#{nekpp_version}/cmake/", "."
+    (testpath/"project.py").write <<~EOS
+      from NekPy.LibUtilities import PointsKey, PointsType, BasisKey, BasisType
+      from NekPy.StdRegions import StdQuadExp
+      import numpy as np
+      ptsKey   = PointsKey(9, PointsType.GaussLobattoLegendre)
+      basisKey = BasisKey(BasisType.Modified_A, 8, ptsKey)
+      quadExp  = StdQuadExp(basisKey, basisKey)
+      x, y     = quadExp.GetCoords()
+      fx       = np.sin(x) * np.cos(y)
+      proj     = quadExp.FwdTrans(fx)
+      assert np.allclose(fx, quadExp.BwdTrans(proj))
+    EOS
+
+    (testpath/"input.xml").write <<~EOS
+      <NEKTAR>
+          <GEOMETRY DIM="2" SPACE="2">
+              <VERTEX COMPRESSED="B64Z-LittleEndian" BITSIZE="64">eJxjYEAGD/aj0gwMjAzYAEKeCas8AjDj0AcDLNjNt4exWLG7Dy7PhlUfwh52HObCAAd2/XB1AAerEJkA</VERTEX>
+              <EDGE COMPRESSED="B64Z-LittleEndian" BITSIZE="64">eJxjYEAFjDhoJhw0Mw4aBljQ1MP4rDj4bGh8mHnsaO6BqeNA48PUcaLxYfZzoYnD9HOj8WHuAgBAqACe</EDGE>
+              <ELEMENT>
+                  <Q COMPRESSED="B64Z-LittleEndian" BITSIZE="64">eJxjYEAFjFCaCUoz4xBngdKsUJoNTZ4dSnNAaU40c5jRxLmgNDea+QAUwABZ</Q>
+              </ELEMENT>
+              <COMPOSITE>
+                  <C ID="0"> Q[0-3] </C>
+                  <C ID="1"> E[0,3,5,6,7,8,10,11] </C>
+              </COMPOSITE>
+              <DOMAIN> C[0] </DOMAIN>
+          </GEOMETRY>
+          <EXPANSIONS>
+              <E COMPOSITE="C[0]" NUMMODES="9" TYPE="MODIFIED" FIELDS="u" />
+          </EXPANSIONS>
+          <CONDITIONS>
+              <PARAMETERS> <P> Lambda = 1 </P> </PARAMETERS>
+              <VARIABLES> <V ID="0"> u </V> </VARIABLES>
+              <BOUNDARYREGIONS> <B ID="0"> C[1] </B> </BOUNDARYREGIONS>
+              <BOUNDARYCONDITIONS>
+                  <REGION REF="0">
+                      <D VAR="u" VALUE="sin(PI*x)*sin(PI*y)" />
+                  </REGION>
+              </BOUNDARYCONDITIONS>
+              <FUNCTION NAME="Forcing">
+                  <E VAR="u" VALUE="-(Lambda + 2*PI*PI)*sin(PI*x)*sin(PI*y)" />
+              </FUNCTION>
+              <FUNCTION NAME="ExactSolution">
+                  <E VAR="u" VALUE="sin(PI*x)*sin(PI*y)" />
+              </FUNCTION>
+          </CONDITIONS>
+      </NEKTAR>
+    EOS
+    system "cmake", "-DNektar++_DIR=#{lib}/nektar++/cmake/", "."
     system "make"
     assert (`./helm input.xml`.to_f < 1.0e-8)
+    python = Formula["python@3.9"]
+    system python.bin/"python3", "project.py"
   end
 end
+
+__END__
+diff --git a/cmake/NektarCommon.cmake b/cmake/NektarCommon.cmake
+index 01274b4fe..e14263b46 100644
+--- a/cmake/NektarCommon.cmake
++++ b/cmake/NektarCommon.cmake
+@@ -291,8 +291,14 @@ MACRO(ADD_NEKPY_LIBRARY name)
+     TARGET_LINK_LIBRARIES(_${name}
+         ${Boost_SYSTEM_LIBRARY}
+         ${BOOST_PYTHON_LIB}
+-        ${BOOST_NUMPY_LIB}
+-        ${PYTHON_LIBRARIES})
++        ${BOOST_NUMPY_LIB})
++
++    IF(APPLE)
++        SET_TARGET_PROPERTIES(_${name} PROPERTIES
++            LINK_FLAGS "-undefined dynamic_lookup")
++    ELSE()
++        TARGET_LINK_LIBRARIES(_${name} ${PYTHON_LIBRARIES})
++    ENDIF()
+ 
+     IF (NEKPY_LIBDEPENDS)
+         TARGET_LINK_LIBRARIES(_${name} ${NEKPY_LIBDEPENDS})
+diff --git a/cmake/ThirdPartyHDF5.cmake b/cmake/ThirdPartyHDF5.cmake
+index c062dd833..a1983abf4 100644
+--- a/cmake/ThirdPartyHDF5.cmake
++++ b/cmake/ThirdPartyHDF5.cmake
+@@ -69,6 +69,10 @@ IF (NEKTAR_USE_HDF5)
+         MESSAGE(STATUS "Found HDF5: ${HDF5_LIBRARIES}")
+         SET(HDF5_CONFIG_INCLUDE_DIR ${HDF5_INCLUDE_DIRS})
+         ADD_CUSTOM_TARGET(hdf5-1.8.16 ALL)
++
++        IF(HDF5_VERSION VERSION_GREATER_EQUAL 1.10.0)
++            ADD_DEFINITIONS(-DH5_USE_110_API)
++        ENDIF()
+     ENDIF()
+ 
+     MARK_AS_ADVANCED(HDF5_LIBRARIES)
